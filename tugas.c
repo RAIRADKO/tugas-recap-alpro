6633320a203ac8f3bf0037380735c6dc0b7fa33f
Rahmat Irfan Adie Purwatmoko <162290027+RAIRADKO@users.noreply.github.com>
2024-03-05 18:00:41 +0700

    add tugas file

--- tugas.c
+++ tugas.c
@@ -1,62 +1,57 @@
-#include ...
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-// Ini fungsi buat men-generate bilangan acak antara min dan max
 int bilanganAcak(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
 
-// Ini fungsi buat ngecek tebakannya dah bener apa belom
 int periksaTebakan(int bilangan, int tebakan) {
     if (tebakan == bilangan) {
         printf("Selamat! Kamu menebak bilangan yang benar.\n");
-        return 1; // Return 1 kalo tebakannya dah bener
-    } ... (tebakan < bilangan) {
+        return 1;
+    } else if (tebakan < bilangan) {
         printf("Terlalu rendah! Coba lagi.\n");
     } else {
         printf("Terlalu tinggi! Coba lagi.\n");
     }
-    return 0; // Return 0 kalo masi blm bener
+    return 0; 
 }
 
 int main() {
     int bilanganMin = 1;
     int bilanganMax = 100;
     int bilanganTarget;
-    int ...
+    int tebakan;
     int percobaan = 0;
     char mainLagi;
 
     srand(time(NULL));
 
     printf("Selamat datang praktikan tercinta di Permainan Tebak Bilangan!\n");
-    printf("Aku udah milih sebuah bilangan antara %d dan %d. Kira kira kamu bisa ga yaaaa nebaknya?\n", bilanganMin, bilanganMax);
+    printf("Aku sudah memilih sebuah bilangan antara %d dan %d. Kira-kira kamu bisa menebaknya?\n", bilanganMin, bilanganMax);
 
-    // Memanggil fungsi bilanganAcak berdasarkan nilai min dan max yang telah ditentukan
-    ... = bilanganAcak(bilanganMin, bilanganMax);
+    bilanganTarget = bilanganAcak(bilanganMin, bilanganMax);
 
     do {
-        printf("Masukan tebakanmu: ");
+        printf("Masukkan tebakanmu: ");
         scanf("%d", &tebakan);
-        ...++;
+        percobaan++;
 
-        // Memanggil fungsi periksaTebakan berdasarkan tebakan yang kamu masukan
         if (periksaTebakan(bilanganTarget, tebakan)) {
             printf("Kamu melakukan %d percobaan.\n", percobaan);
             printf("Apakah kamu ingin bermain lagi? (Y/T): ");
-            scanf(..., &mainLagi);
-            if (mainLagi == 'Y' || ... == 'y') {
-                // Men-generate bilangan baru untuk permainan selanjutnya
+            scanf(" %c", &mainLagi);
+            if (mainLagi == 'Y' || mainLagi == 'y') {
+                
                 bilanganTarget = bilanganAcak(bilanganMin, bilanganMax);
-                ... = 0;
+                percobaan = 0;
                 printf("\nSaya telah memilih bilangan baru. Ayo bermain lagi!\n");
             } else {
                 printf("Terima kasih telah bermain. Selamat tinggal!\n");
-                ... // Keluar dari perulangan
+                break; 
             }
         }
-    } while (...);
-
+    } while (1); 
     return 0;
 }
